87e3a12100b8655c84cfe1c89b3f5ae49be55a4a
Gabriel <damore120@gmail.com>
2023-02-01 19:41:25 -0300

    first commit

---
+++ src/test/java/br/com/gabriel/openfeature/OpenfeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.openfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
